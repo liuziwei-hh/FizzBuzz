26c1e9c876a588bd5967a0681d62c8037fe44877
LZW <1312299240@qq.com>
2020-06-15 14:57:25 +0800

    finish codeFormat and correct

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,18 +2,18 @@ package com.thoughtworks;
 
 public class FizzBuzz {
 
- public String say(int i){
-     boolean IsDevidedBy3=i%3==0;
-     boolean IsDevidedBy5=i%5==0;
-     if(IsDevidedBy3&&IsDevidedBy5){
-         return "FizzBuzz";
-     }
-     if (IsDevidedBy3){
-         return "Fizz";
-     }
-     if(IsDevidedBy5){
-         return "Buzz";
-     }
-     return String.valueOf(i);
- }
+    public String say(int i) {
+        boolean IsDevidedBy3 = i % 3 == 0;
+        boolean IsDevidedBy5 = i % 5 == 0;
+        if (IsDevidedBy3 && IsDevidedBy5) {
+            return "FizzBuzz";
+        }
+        if (IsDevidedBy3) {
+            return "Fizz";
+        }
+        if (IsDevidedBy5) {
+            return "Buzz";
+        }
+        return String.valueOf(i);
+    }
 }
